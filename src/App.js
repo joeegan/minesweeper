300f7a13fc1d22e952ff0403a818f025cc086386
Joe Egan <joseph.egan@ig.com>
2017-05-10 13:13:29 +0100

    use functional components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import './App.css';
 import _ from 'lodash';
 
-const grid = _.times(9)
-              .map(i => (
-                <ul className='row'>
-                  {_.times(9).map(i => <li className='cell'></li>)}
-                </ul>
-              ))
+const gridSize = 9;
 
-class App extends Component {
-  render() {
-    return (
-      <div className='wrapper'>
-        <div className='scoreBoard'>
-          <div className='minesLeft digits'>010</div>
-          <div className='face face_smiling'>
-            <div className='face-inner'>😃</div>
-          </div>
-          <div className='timeLeft digits'>000</div>
-        </div>
-        <div className='grid'>{grid}</div>
+const grid = _.times(gridSize)
+              .map(i => (
+                <ul className='row' key={i}>
+                  {_.times(gridSize).map(j =>
+                    <li className='cell' key={j}></li>
+                  )}
+                </ul>
+              ));
+
+const App = () => (
+  <div className='wrapper'>
+    <div className='scoreBoard'>
+      <div className='minesLeft digits'>010</div>
+      <div className='face face_smiling'>
+        <div className='face-inner'>😃</div>
       </div>
-    );
-  }
-}
+      <div className='timeLeft digits'>000</div>
+    </div>
+    <div className='grid'>{grid}</div>
+  </div>
+);
 
 export default App;
